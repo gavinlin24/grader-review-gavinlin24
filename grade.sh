c5d80ca7169f7cf853e7e4bfce78c343f2b221d1
gavinlin24 <gavinlin24@gmail.com>
2023-11-06 14:48:37 -0800

    first commit

--- grade.sh
+++ grade.sh
@@ -6,8 +6,23 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
-echo 'Finished cloning'
+echo "finished cloning"
 
+path=`find student-submission -name ListExamples.java`
+
+if [[ -e $path ]]
+then
+    echo "ListExamples.java found"
+else
+    echo "ListExamples.java not found"
+    exit
+fi
+
+cp -r student-submission/ListExamples.java grading-area
+cp -r TestListExamples.java grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
