6b3b7955626792b65bd402754c04a116da3fb510
gavinlin24 <gavinlin24@gmail.com>
2023-11-27 14:25:04 -0800

    ieng6

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -17,5 +18,39 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testMerge(){
+    ArrayList<String> input1 = new ArrayList<String>();
+    ArrayList<String> input2 = new ArrayList<String>();
+    input1.add("a");
+    input1.add("d");
+    input2.add("b");
+    input2.add("c");
+
+    ArrayList<String> output = new ArrayList<String>();
+    output.add("a");
+    output.add("b");
+    output.add("c");
+    output.add("d");
+
+    assertEquals(output, ListExamples.merge(input1, input2));
+  }
+
+  @Test
+  public void testFilter(){
+        ArrayList<String> input1 = new ArrayList<String>();
+        input1.add("MOON");
+        input1.add("sun");
+       
+        ListExamples.filter(input1, new IsMoon());
+
+        ArrayList<String> output = new ArrayList<String>();
+        output.add("MOON");
+
+        assertEquals(output, input1);
+
+
   }
 }
--- grade.sh
+++ grade.sh
@@ -18,14 +18,23 @@ else
     exit
 fi
 
-cp -r student-submission/ListExamples.java grading-area
-cp -r TestListExamples.java grading-area
-
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
-
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp -r student-submission/ListExamples.java grading-area
+cp -r TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? != 0 ]]
+then
+    echo "ListExamples.java could not compile"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
